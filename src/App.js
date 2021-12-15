efd8f6f985916cc95bce508522e5d21a04ebe4fc
hxxtae <fkdlxmfkdl@gmail.com>
2021-12-15 20:21:54 +0900

    :wrench: Implement state & props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import { Subject, TOC, Content } from './components';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      toc: [
+        { id: 1, link: 'data-1', content: "HTML" },
+        { id: 2, link: 'data-2', content: "CSS" },
+        { id: 3, link: 'data-3',content: "JavaScript" }
+      ],
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
-        <TOC title="Web Menu" list_1="HTML" list_2="CSS" list_3="Javascript"></TOC>
-        <Content title="HTML" content="HTML is HyperText Markup Language."></Content>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}></Subject>
+        <TOC
+          data={this.state.toc}></TOC>
+        <Content
+          title="HTML"
+          content="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
 }
 
 export default App;
+
+
+/*
+  - props : key
+  - state : value
+
+  ※ 부모에서의 state 를 자식에게 전달 할 때에는 props 라는 것을 통해 값을 전달
+*/
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
   render() {
+    const list = [];
+    const data = this.props.data;
+    let i = 0;
+    while (i < data.length) {
+      list.push(
+        <li key={data[i].id}>
+          <a href={'/comment/' + data[i].link}>{data[i].content}</a>
+        </li>);
+      i += 1;
+    }
+    console.log(list)
     return (
       <nav>
         <h2>{this.props.title}</h2>
         <ul>
-          <li><a href="/">{this.props.list_1}</a></li>
-          <li><a href="/">{this.props.list_2}</a></li>
-          <li><a href="/">{this.props.list_3}</a></li>
+          {list}
         </ul>
       </nav>
     );
